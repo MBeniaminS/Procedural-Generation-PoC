829c87f02cf68b22d04c95d59d5a18136628a6c1
MBeniaminS <mateuszsykut27@gmail.com>
2025-01-29 18:35:24 +0000

    cheez

--- Uni-Applied-Programming-Shooter/Assets/Scripts/Player/PlayerLook.cs
+++ Uni-Applied-Programming-Shooter/Assets/Scripts/Player/PlayerLook.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerLook : MonoBehaviour
 {
+    private Camera mainCam;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mainCam = FindFirstObjectByType<Camera>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 mousePosition = mainCam.ScreenToWorldPoint();
+        Ray cameraRay = mainCam.ScreenPointToRay();
     }
 }
--- Uni-Applied-Programming-Shooter/Assets/Scripts/Player/PlayerMovement.cs
+++ Uni-Applied-Programming-Shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 2.5f;
+    private Rigidbody rb;
+
+
+    private InputAction moveAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 inputVector = moveAction.ReadValue<Vector2>();
+        Vector3 moveForce = new Vector3(inputVector.x, 0f, inputVector.y);
+        rb.AddForce(moveForce * moveSpeed);
+
+        print(rb.linearVelocity);
     }
+
 }
