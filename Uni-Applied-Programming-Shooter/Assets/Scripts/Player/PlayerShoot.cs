c827a89956c2e5d88ad1d1558285ae61fa5e3cb3
MBeniaminS <mateuszsykut27@gmail.com>
2025-02-04 14:55:46 +0000

    LEARNT ABOUT REQUIRE COMPOENNT

--- Uni-Applied-Programming-Shooter/Assets/Scripts/Player/PlayerShoot.cs
+++ Uni-Applied-Programming-Shooter/Assets/Scripts/Player/PlayerShoot.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(ShootProjectile))]
 public class PlayerShoot : MonoBehaviour
 {
+    [SerializeField] private GameObject projectilePrefab;
+
+    bool hasFired;
+
+    private InputAction fireAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        fireAction = InputSystem.actions.FindAction("Fire");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (fireAction.IsPressed() == true && hasFired == false)
+        {
+            // fire
+            hasFired = true;
+        }
+        if (fireAction.WasReleasedThisFrame())
+        {
+            hasFired = false;
+        }
     }
 }
